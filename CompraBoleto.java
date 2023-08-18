1c744b1b1287879a0b8479f6262846aa8659661a
Javiervalladares1 <javivalladares10@icloud.com>
2023-08-18 09:09:43 -0600

    Commit Camila

--- CompraBoleto.java
+++ CompraBoleto.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Compraboleto {
+public class CompraBoleto {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
